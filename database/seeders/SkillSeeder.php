c9ea2881f081887a54b350b14ac1b35aef1b07dd
Faheem2407 <faheem24072000@gmail.com>
2025-09-30 17:57:32 +0600

    updated the skill seeder

--- database/seeders/SkillSeeder.php
+++ database/seeders/SkillSeeder.php
@@ -9,11 +9,16 @@ class SkillSeeder extends Seeder
 {
     public function run()
     {
+        Skill::create(['title' => 'PHP(OOP)']);
         Skill::create(['title' => 'Laravel']);
-        Skill::create(['title' => 'PHP']);
+        Skill::create(['title' => 'RESTful APIs']);
+        Skill::create(['title' => 'AI API Ingeration']);
+        Skill::create(['title' => 'Authentication (JWT / Breeze / Sanctum / Passport)']);
+        Skill::create(['title' => 'Database (MySQL / SQLite / MongoDB / PostgreSQL)']);
         Skill::create(['title' => 'JavaScript']);
-        Skill::create(['title' => 'Vue.js']);
-        Skill::create(['title' => 'HTML/CSS']);
-        Skill::create(['title' => 'MySQL']);
+        Skill::create(['title' => 'Bootstrap']);
+        Skill::create(['title' => 'TailwindCSS']);
+        Skill::create(['title' => 'Git & GitHub']);
+        Skill::create(['title' => 'cPanel']);
     }
 }
